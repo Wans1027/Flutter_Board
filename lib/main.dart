e35ba6c33311c4fbc04a0022b5a86dada542333f
Wan1027 <rladhkstmd10@daum.net>
2023-03-31 21:55:20 +0900

    testScreen

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_board/screen/test_screen.dart';
+import 'package:flutter_board/services/api_service.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
